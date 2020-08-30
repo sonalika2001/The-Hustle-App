b9bd497ca6ddd367282773ae09bddd00d0345a5e
Sonalika Sahoo <sonalika2405@gmail.com>
2020-08-30 12:40:36 +0530

    initializing todo app

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFEDBD0),
+      body: ,
+    );
+  }
+}
