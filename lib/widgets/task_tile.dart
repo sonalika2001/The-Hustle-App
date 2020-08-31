b9c1cd690b5725e357fea5de9803540b35d83ef2
Sonalika Sahoo <sonalika2405@gmail.com>
2020-08-31 18:47:34 +0530

    Lifted state for checklist

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+ 
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'This is a task.',
         style: TextStyle(
+          decoration: isChecked? TextDecoration.lineThrough:null,
           fontFamily: 'Schoolbell',
           fontSize: 20,
         ),
+        
       ),
       leading: Icon(
         Icons.whatshot,
         color: Color(0xff442C2E),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-      ),
+      trailing: TaskCheckbox(checkBoxState: isChecked,toggleCheckBoxState: (bool checkBoxState){
+        setState(() {
+          isChecked=checkBoxState;
+        });
+      },),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+  TaskCheckbox({this.checkBoxState,this.toggleCheckBoxState});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Color(0xff442C2E),
+      checkColor: Color(0xFFFEEAE6),
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
 
-
+class _TasksListState extends State<TasksList> {
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
+        TaskTile(),
+        TaskTile(),
         TaskTile(),
       ],
     );
